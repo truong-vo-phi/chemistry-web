16d2707e20b6c4e2f0170e2729dd3e094bb40c06
kDorega <voquangdangkhoa123@gmail.com>
2026-05-18 20:36:13 +0700

    model MVC

---
+++ ChemistryV1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ChemistryV1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
